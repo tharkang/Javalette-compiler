d4163bf3762406f2d85a763615c5a27022c26ea0
Marcin Kurczych <tharkang@gmail.com>
2010-12-17 23:50:05 +0100

    added javacup dependecy

--- project/build/JavaletteProject.scala
+++ project/build/JavaletteProject.scala
@@ -3,4 +3,5 @@ import mk262968.JavaCup;
 
 class JavaletteProject(info: ProjectInfo) extends DefaultProject(info) with JavaCup with extract.BasicSelfExtractingProject {
 	override def installActions = "update" :: "package" :: Nil
+  val javaCup = "net.sf.squirrel-sql.thirdparty-non-maven" % "java-cup" % "0.11a";
 }
